655041ceb746d516f61a90635cecd4c80cfbbba2
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-09-24 15:49:07 -0400

    base tutorial playground

---
+++ script.js
@@ -0,0 +1,7 @@
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("DOMContentLoaded");
+});
